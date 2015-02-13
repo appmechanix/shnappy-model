39b8db8a2abab48ec8c89eb00f5fbbb006762994
Daniel Wylie <daniel@danielwylie.me>
2015-02-14 00:20:07 +1300

    Added initial model

---
+++ test/client.js
@@ -0,0 +1,7 @@
+var model = require('../lib/index');
+
+exports.check_that_user_is_there = function (test) {
+    test.notEqual(model.Client, undefined);
+    test.notEqual(model.Client, null);
+    test.done();
+};
