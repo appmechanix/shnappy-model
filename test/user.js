0a14e509fbf7f2c0e3bf46b95f91b434a6470956
Daniel Wylie <daniel@danielwylie.me>
2015-02-15 17:35:38 +1300

    Sigh. Wrong place.

--- test/user.js
+++ test/user.js
@@ -1,11 +1,8 @@
 var User = require('../lib/index').User;
-
 var mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost/photohosting");
-
-console.log(User);
 
 exports.test_that_can_connect = function (test) {
+    mongoose.connect("mongodb://localhost/photohosting");
     User.find(function (err, data) {
         test.notEqual(null, data);
         mongoose.disconnect();
