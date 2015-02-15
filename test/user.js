5d5f3aeec6454b0009cbb516cd6051cb153db67d
Daniel Wylie <daniel@danielwylie.me>
2015-02-15 17:38:18 +1300

    C'mon NPM

--- test/user.js
+++ test/user.js
@@ -3,10 +3,10 @@ var mongoose = require('mongoose');
 
 exports.test_that_can_connect = function (test) {
     mongoose.connect("mongodb://localhost/photohosting");
+
     User.find(function (err, data) {
         test.notEqual(null, data);
         mongoose.disconnect();
-
         test.done();
     });
 };
