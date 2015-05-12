5981f80e2ccd701f606eeb6c01fdd88cdb9e9002
Daniel Wylie <daniel@danielwylie.me>
2015-05-12 19:19:52 +1200

    Add is public property to client model

--- test/client.tests.js
+++ test/client.tests.js
@@ -6,3 +6,11 @@ exports.check_that_user_is_there = function (test) {
 
     test.done();
 };
+
+exports.check_that_public_isnt_set_by_default = function (test) {
+    var client = new model.Client();
+
+    test.equal(false, client.isPublic);
+
+    test.done();
+};
