515eda5a99a4e6c25f372cbeeaf56e04e1440884
Daniel Wylie <daniel@danielwylie.me>
2015-06-24 22:32:25 +1200

    Add IsFirstLogin method

--- test/clientShareTokenTest.js
+++ test/clientShareTokenTest.js
@@ -18,3 +18,37 @@ exports.check_that_uuid_is_set_on_token_insert = function (test) {
         test.done();
     });
 };
+
+exports.check_that_is_first_login_is_true_on_first_login = function (test) {
+    mongoose.connect("mongodb://localhost/photohosting-test");
+
+    var token = new ClientShareToken({
+        studioId: 'TEST',
+        clientId: 'TEST'
+    });
+
+    token.save(function (err, data) {
+        test.equal(true, data.IsFirstLogin());
+
+        mongoose.disconnect();
+        test.done();
+    });
+};
+
+
+exports.check_that_is_first_login_is_true_on_first_login = function (test) {
+    mongoose.connect("mongodb://localhost/photohosting-test");
+
+    var token = new ClientShareToken({
+        studioId: 'TEST',
+        clientId: 'TEST',
+        timesUsed: 10
+    });
+
+    token.save(function (err, data) {
+        test.equal(false, data.IsFirstLogin());
+
+        mongoose.disconnect();
+        test.done();
+    });
+};
