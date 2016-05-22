f7b3146eed2e35e6f5b88aeb741963f8f8e8e1bb
Daniel Wylie <daniel@danielwylie.me>
2016-05-22 16:02:00 +1200

    Add helper method to client + tests for method

--- test/client.tests.js
+++ test/client.tests.js
@@ -14,3 +14,32 @@ exports.check_that_public_isnt_set_by_default = function (test) {
 
     test.done();
 };
+
+exports.check_that_non_expired_client_is_valid = function (test) {
+    var client = new model.Client();
+    test.equal(false, client.HasExpired());
+
+    test.done();
+};
+
+exports.check_that_half_saved_expiry_is_valid = function (test) {
+    var client = new model.Client();
+    client.expires = true;
+    
+    test.equal(false, client.HasExpired());
+
+    test.done();
+};
+
+exports.check_that_expired_client_is_invalid = function (test) {
+    var client = new model.Client();
+    var d = new Date();
+    d.setDate(d.getDate() - 5);
+
+    client.expires = true;
+    client.expiresOn = d;
+
+    test.equal(true, client.HasExpired());
+
+    test.done();
+};
